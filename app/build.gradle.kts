451f2a7539ad23c1d6f9f6c5f9b7a1ad1f364466
bigbackboom <kodaik@me.com>
2022-11-12 20:23:37 +0900

    コード設定を保存

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,12 @@
-import com.bigbackboom.tryandroidstuff.build.Package
 import com.bigbackboom.tryandroidstuff.build.AppVersion
 import com.bigbackboom.tryandroidstuff.build.Deps
+import com.bigbackboom.tryandroidstuff.build.Package
 
 plugins {
     id("com.android.application")
+    id("kotlin-kapt")
     id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -46,13 +48,24 @@ android {
 
 dependencies {
 
+    // AndroidX
     implementation(Deps.AndroidX.Ktx.core)
     implementation(Deps.AndroidX.appCompat)
     implementation(Deps.AndroidX.design)
     implementation(Deps.AndroidX.constraintLayout)
     implementation(Deps.AndroidX.Ktx.navigationFragment)
     implementation(Deps.AndroidX.Ktx.navigationUI)
+
+    // Dagger(Hilt)
+    implementation(Deps.Dagger.hilt)
+    kapt(Deps.Dagger.hiltCompiler)
+
+    // Test
     testImplementation(Deps.Test.jUnit)
     androidTestImplementation(Deps.AndroidX.Test.jUnit)
     androidTestImplementation(Deps.AndroidX.Test.espresso)
+}
+
+kapt {
+    correctErrorTypes = true
 }
